602aa017a23d431de70ad96485d42fba4b4c3271
maxverein <maxverein@mail.ru>
2017-10-20 19:15:52 +0300

    finish directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   title = 'app';
   headerText = 'Директива';
 
+  items = [1, 2, 3, 4, 5, 6];
+  current = 1;
+
+  onClick (item: number) {
+    console.log(item);
+    this.current = item;
+  }
+
 }
--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,11 +1,15 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
+  // @Input('appBackground') hoverColor: string;
+  @Input() defaultColor; string
+  @Input() hoverColor: string;
+  @Input() highlightColor = 'yellow';
 
-  @HostBinding('style.backgroundColor') background: String;
+  @HostBinding('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
@@ -17,20 +21,30 @@ export class BackgroundDirective implements OnInit {
     nativeElement.style.textAlign = 'center'; // плохой подход
    // nativeElement.style.color = 'white';
     this.renderer.addClass(nativeElement, 'text-color');*/
-    const { nativeElement } = this.element;
-    nativeElement.style.textAlign = 'center'; // плохой подход
+
+  //  const { nativeElement } = this.element;
+  //  nativeElement.style.textAlign = 'center'; // плохой подход
+    this.background = this.defaultColor;
   }
 
   @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
     const { nativeElement } = this.element;
   //  this.renderer.setStyle(nativeElement, 'background-color', 'blue');
    this.renderer.addClass(nativeElement, 'text-color');
-    this.background = 'blue';
+  //  this.background = 'blue';
+   // this.background = this.hoverColor;
+      this.background = this.hoverColor;
   }
 
-  @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
+/*  @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
     const { nativeElement } = this.element;
     this.renderer.removeClass(nativeElement, 'text-color');
     this.background = 'transparent';
-  }
+  }*/
+
+/*  @HostListener('mouseleave') mouseLeave() {
+    const { nativeElement } = this.element;
+    this.renderer.removeClass(nativeElement, 'text-color');
+    this.background = 'transparent';
+  }*/
 }
